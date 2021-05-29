8d44f5585e175ea9fa3e1175923c3d2986cdaa2a
Laptop Dexter <truongdailuc89@gmail.com>
2021-05-29 22:14:44 +0700

    commited

--- wp-content/themes/rnd/index.php
+++ wp-content/themes/rnd/index.php
@@ -46,14 +46,14 @@
       <h2 class="welcome__title">WELCOME</h2>
       <p class="welcome__desc">Welcome to SHYNH PREMIUM. When you walk through our doors, you will immediately feel a sense of relaxation. Soothing colors, soft music and lovely aromas transport you to a place of peace and serenity. Customers will enjoy a 5-star-class relaxation space as well as excellent services at SHYNH PREMIUM.  Proud to be a top 1 beauty clinic leading high-tech beauty trend nowadays, honored to accompany millions of women in the beauty journey.</p>
       <div class="welcome__slider-showcase-wrapper">
-        <div id="xxxwelcome__carousel" class="welcome__slider">
+        <div id="welcome__carousel" class="welcome__slider owl-carousel owl-theme">
           <div class="welcome__item">
             <div class="welcome__item__wrapper">
               <img src="<?php echo get_theme_file_uri('/images/wslider1.webp') ?>" alt="alt" title="title" class="welcome__item__img">
               <h3 class="welcome__item__desc">DA NANG CITY</h3>
             </div>
           </div>   
-          <!-- <div class="welcome__item">
+          <div class="welcome__item">
             <div class="welcome__item__wrapper">
               <img src="<?php echo get_theme_file_uri('/images/wslider2.webp') ?>" alt="alt" title="title" class="welcome__item__img">
               <h3 class="welcome__item__desc">DA NANG CITY</h3>
@@ -64,7 +64,7 @@
               <img src="<?php echo get_theme_file_uri('/images/wslider3.webp') ?>" alt="alt" title="title" class="welcome__item__img">
               <h3 class="welcome__item__desc">DA NANG CITY</h3>
             </div>
-          </div>    -->
+          </div>   
         </div>   
         <div class="welcome__showcase">
             <span class="welcome__showcase__item">
