9af410a46f4179194cc061f0bd75f1239fd15d1a
Laptop Dexter <truongdailuc89@gmail.com>
2021-06-02 09:34:15 +0700

    home page

--- wp-content/themes/rnd/inc/short-code-view/main_slider.view.php
+++ wp-content/themes/rnd/inc/short-code-view/main_slider.view.php
@@ -1,9 +1,9 @@
 <session class="main-slider">
   <div class="main-slider__wrapper">
-    <div class="main-slider__item">
+    <a href="#" class="main-slider__item">
       <picture class="main-slider__item__img-wrapper ">          
-          <source media="(min-width:767px)" src="<?php echo imageEncode('images/slider1.webp');?>" title="title" alt="alt" width="" height="">
-          <img src="<?php echo imageEncode('images/mslider1.webp');?>" title="title" alt="alt" width="" height="">
+          <source media="(min-width:992px)" srcset="<?php echo imageEncode('images/slider1.webp');?>" title="title" alt="alt">
+          <img src="<?php echo imageEncode('images/mslider1.webp');?>" title="title" alt="alt" >
       </picture>
       <div class="main-slider__item__info">
           <h1 class="main-slider__item__heading">SHYNH PREMIUM</h1>
@@ -11,6 +11,6 @@
           <p class="main-slider__item__desc">with the mission of bringing customers the highest quality beauty experience.</p>
           <a href="#" class="main-slider__item__book-now">BOOK NOW</a>
         </div>
-    </div>
+    </a>
   </div>
 </session>
